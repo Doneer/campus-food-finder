cab85c602fe1eac8c3a5b309f2445f835ef41527
Daniyar Zhumatayev <253857@edu.p.lodz.pl>
2025-05-14 20:10:52 +0200

    Updates

---
+++ precache-manifest.764a0c33733650fac32542af1ae55425.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "baf2db256bdcbcf09810351a60716bd6",
+    "url": "/campus-food-finder/index.html"
+  },
+  {
+    "revision": "b2b4025163dfb483546f",
+    "url": "/campus-food-finder/static/css/main.49ca8e89.chunk.css"
+  },
+  {
+    "revision": "09fead582106488ffb7a",
+    "url": "/campus-food-finder/static/js/2.aca736dc.chunk.js"
+  },
+  {
+    "revision": "b62d8d853f9b6d327553199311cfed18",
+    "url": "/campus-food-finder/static/js/2.aca736dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "306f288283ecc955e988",
+    "url": "/campus-food-finder/static/js/3.903755cb.chunk.js"
+  },
+  {
+    "revision": "b2b4025163dfb483546f",
+    "url": "/campus-food-finder/static/js/main.6e408e08.chunk.js"
+  },
+  {
+    "revision": "8817d7453daab84e0878",
+    "url": "/campus-food-finder/static/js/runtime-main.51487d58.js"
+  }
+]);
